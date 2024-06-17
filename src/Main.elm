7f6c7ad6fada20723b47cf2cf5343a8f13c43405
Taylor Troesh <surprisetalk@gmail.com>
2024-06-16 20:17:39 -0700

    😲

--- src/Main.elm
+++ src/Main.elm
@@ -5,6 +5,7 @@ import Browser exposing (Document)
 import Html as H exposing (Html, text)
 import Html.Attributes as A exposing (style)
 import Html.Events as A
+import Html.Lazy as H
 import Process
 import Regex exposing (Regex)
 import Task
@@ -22,6 +23,8 @@ iif c t f =
 type Msg
     = WriteCell Int String
     | CellsWritten Rect
+    | CellsSelecting Index
+    | CellsSelected Index
 
 
 type alias Cell =
@@ -40,6 +43,7 @@ type RuleDef
     = HttpFetch String
     | JS String
     | JsonPath String
+    | Copy
     | Sum
     | Product
     | Mean
@@ -71,8 +75,8 @@ type alias Rect =
 
 
 type alias Model =
-    { selected : Maybe Query
-    , editing : Maybe ( Index, Cell )
+    { selected : Query
+    , editing : ( Index, Cell )
     , sheet : Sheet
     , rules : List Rule
     , newRule : ( Int, RuleDef )
@@ -92,8 +96,8 @@ main =
 
 init : () -> ( Model, Cmd Msg )
 init _ =
-    ( { selected = Nothing
-      , editing = Nothing
+    ( { selected = Rect ( ( 8, 7 ), ( 12, 13 ) )
+      , editing = ( ( -1, -1 ), "" )
       , sheet = { cols = 10, cells = Array.initialize (10 * 100) (\i -> String.fromInt (modBy 10 i) ++ "," ++ String.fromInt (i // 10)) }
       , rules =
             [ { query = Rect ( ( 0, 0 ), ( 3, 4 ) )
@@ -109,9 +113,25 @@ init _ =
     )
 
 
+inc : Index -> Index
+inc =
+    translate ( 1, 1 )
+
+
 translate : Vector -> Index -> Index
 translate ( x, y ) ( x_0, y_0 ) =
     ( x_0 + x, y_0 + y )
+
+
+match : Query -> Index -> Cell -> Bool
+match q ( xi, yi ) s =
+    case q of
+        Rect ( ( xa, ya ), ( xb, yb ) ) ->
+            xa <= xi && xi < xb && ya <= yi && yi < yb
+
+        -- TODO
+        Pattern _ ->
+            False
 
 
 toFlatIndex : Int -> Index -> Int
@@ -121,7 +141,7 @@ toFlatIndex cols ( x, y ) =
 
 fromFlatIndex : Int -> Int -> Index
 fromFlatIndex cols i =
-    ( i // cols, modBy cols i )
+    ( modBy cols i, i // cols )
 
 
 intersect : Rect -> Rect -> Rect
@@ -200,16 +220,56 @@ update msg ({ sheet } as model) =
                         subsheets
             )
 
+        CellsSelecting a ->
+            ( { model | selected = Rect ( a, a ) }, Cmd.none )
+
+        CellsSelected b ->
+            case model.selected of
+                Rect ( a, _ ) ->
+                    ( { model | selected = Rect ( a, inc b ) }, Cmd.none )
+
+                Pattern _ ->
+                    ( { model | selected = Rect ( b, b ) }, Cmd.none )
+
 
 view : Model -> Document Msg
 view model =
     { title = "scrapsheets"
     , body =
-        [ H.div [ style "display" "grid", style "grid-template-columns" "2fr 1fr" ]
-            [ H.div [ style "display" "grid", style "grid-template-columns" (String.repeat model.sheet.cols "1fr ") ] <|
-                Array.toList <|
-                    Array.indexedMap (\i cell -> H.tr [] [ H.td [ A.onClick (WriteCell i (cell ++ "!")) ] [ text cell ] ]) <|
-                        model.sheet.cells
+        -- { selected : Query
+        -- , editing : ( Index, Cell )
+        -- , sheet : Sheet
+        -- , rules : List Rule
+        -- , newRule : ( Int, RuleDef )
+        -- , frameDuration : Float
+        -- }
+        [ H.node "style" [] [ text ".cell:hover { background: #eee;  }" ]
+        , H.div
+            [ style "display" "grid"
+            , style "grid-template-columns" "2fr 1fr"
+            , style "user-select" "none"
+            , style "-webkit-user-select" "none"
+            , style "cursor" "pointer"
+            ]
+            [ H.lazy
+                (H.div
+                    [ style "display" "grid"
+                    , style "grid-template-columns" (String.repeat model.sheet.cols "1fr ")
+                    ]
+                    << Array.toList
+                    << Array.indexedMap
+                        (\i cell ->
+                            H.div
+                                [ A.class "cell"
+                                , style "background" (iif (match model.selected (fromFlatIndex model.sheet.cols i) cell) "#ddd" "")
+                                , A.onClick (WriteCell i (cell ++ "!"))
+                                , A.onMouseDown (CellsSelecting (fromFlatIndex model.sheet.cols i))
+                                , A.onMouseUp (CellsSelected (fromFlatIndex model.sheet.cols i))
+                                ]
+                                [ text cell ]
+                        )
+                )
+                model.sheet.cells
             , H.div [ style "display" "flex", style "flex-direction" "column" ] <|
                 List.map (\rule -> H.div [] [ text (Debug.toString rule) ]) <|
                     model.rules
