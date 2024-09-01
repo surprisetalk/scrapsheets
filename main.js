4d21a1f9fad9ce74a30b7f941e63c1b31ad08c3b
Taylor Troesh <surprisetalk@gmail.com>
2024-09-01 16:13:51 -0700

    💨

--- main.js
+++ main.js
@@ -2,22 +2,43 @@ import van from "https://cdn.jsdelivr.net/gh/vanjs-org/van/public/van-1.5.2.min.
 
 const { div, table, tbody, tr, td, a, input, span, aside, main } = van.tags;
 
-const Cell = () => "TODO";
+const Cell = () => {
+  return input({ disabled: true, onblur: e => (e.target.disabled = true) });
+};
 
 const Sheet = rows =>
-  table(tbody(rows.map(row => tr(row.map(cell => td(Cell(cell)))))));
+  table(
+    { class: "sheet" },
+    tbody(
+      rows.map(row =>
+        tr(
+          row.map(cell =>
+            td(
+              {
+                onclick: e => {
+                  const x = e.target.closest("td").querySelector("input");
+                  x.disabled = false;
+                  x.focus();
+                },
+              },
+              Cell(cell)
+            )
+          )
+        )
+      )
+    )
+  );
 
-// improve perf: https://vanjs.org/tutorial#stateful-binding
+const sheet = van.state(new Array(20).fill(new Array(10).fill("")));
+
 van.add(
   document.body,
+  div({ id: "clipboard" }),
   main(
-    div(), // clipboard
-    div(
-      Sheet(new Array(20).fill(new Array(10).fill(""))),
-      aside(
-        div(), // channels
-        div() // rules
-      )
+    Sheet(sheet.val),
+    aside(
+      div(), // channels
+      div() // rules
     )
   )
 );
