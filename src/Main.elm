46ffee90c9c194a610d7a799fef7363600e29bda
Taylor Troesh <surprisetalk@gmail.com>
2024-09-04 06:55:13 -0700

    🏡

---
+++ src/Main.elm
@@ -0,0 +1,248 @@
+module Main exposing (main)
+
+---- IMPORTS ------------------------------------------------------------------
+
+import Array exposing (Array)
+import Browser
+import Browser.Navigation as Nav
+import Date exposing (Date)
+import Dict exposing (Dict)
+import Html as H exposing (Html, text)
+import Html.Attributes as A exposing (..)
+import Html.Events as A exposing (..)
+import Html.Lazy as H
+import Html.Style as S
+import Http
+import Json.Decode as D
+import Json.Encode as E
+import Regex exposing (Regex)
+import Time exposing (Month(..))
+import Url exposing (Url)
+
+
+
+---- HELPERS ------------------------------------------------------------------
+
+
+ls : a -> List a
+ls =
+    List.singleton
+
+
+iif : Bool -> a -> a -> a
+iif c t f =
+    if c then
+        t
+
+    else
+        f
+
+
+flip : (a -> b -> c) -> (b -> a -> c)
+flip f a b =
+    f b a
+
+
+
+---- MAIN ---------------------------------------------------------------------
+
+
+main : Program () Model Msg
+main =
+    Browser.application
+        { init = init
+        , update = update
+        , view = view
+        , subscriptions = subscriptions
+        , onUrlChange = UrlChanged
+        , onUrlRequest = LinkClicked
+        }
+
+
+
+---- SUBSCRIPTIONS ------------------------------------------------------------
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Sub.none
+
+
+
+---- MODEL --------------------------------------------------------------------
+
+
+type alias SheetId =
+    String
+
+
+type alias Input valid data =
+    { valid : valid
+    , default : data
+    , data : data
+    }
+
+
+type Col
+    = Numbers (Array Float)
+    | Strings (Array String)
+    | Images (Array String)
+    | Links (Array String)
+    | Datepickers (Array (Input () Date))
+    | Checkboxes (Array (Input () Bool))
+    | Sliders (Array (Input ( Float, Float ) Float))
+    | Fields (Array (Input Regex String))
+    | Chart (Array ( Float, Float ))
+
+
+type alias Sheet =
+    { transpose : Bool
+    , rows : Int
+    , cols : Array ( String, Col )
+    }
+
+
+type alias Model =
+    -- The output of `code` completely determines the shape and content of `sheet`.
+    { sheets : Dict SheetId { code : String, sheet : Result String Sheet }
+
+    -- the row beneath the sheet opens up with its code and a pointer and tools
+    , shelf : List (List SheetId) -- use empty sheets as row/col spacers
+    }
+
+
+
+---- INIT ---------------------------------------------------------------------
+
+
+init : () -> Url -> Nav.Key -> ( Model, Cmd Msg )
+init _ url _ =
+    ( { sheets =
+            Dict.empty
+                |> Dict.insert "EXAMPLE"
+                    { code = "TODO"
+                    , sheet =
+                        Ok
+                            { transpose = False
+                            , rows = 4
+                            , cols =
+                                Array.fromList
+                                    [ ( "Col", Numbers <| Array.fromList [ 1, 2, 3, 4 ] )
+                                    , ( "Col", Numbers <| Array.fromList [ 5, 6, 7, 8 ] )
+                                    ]
+                            }
+                    }
+      , shelf =
+            [ [ "EXAMPLE" ]
+            ]
+      }
+    , Cmd.none
+    )
+
+
+
+---- MESSAGES -----------------------------------------------------------------
+
+
+type Msg
+    = NoOp
+    | UrlChanged Url
+    | LinkClicked Browser.UrlRequest
+
+
+
+---- UPDATE -------------------------------------------------------------------
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        _ ->
+            ( model, Cmd.none )
+
+
+
+---- VIEW ---------------------------------------------------------------------
+
+
+viewCell : Int -> Col -> Maybe (Html Msg)
+viewCell i col =
+    case col of
+        Numbers xs ->
+            xs |> Array.get i |> Maybe.map (String.fromFloat >> text)
+
+        Strings xs ->
+            xs |> Array.get i |> Maybe.map text
+
+        Images xs ->
+            xs |> Array.get i |> Maybe.map (\src -> H.img [ A.src src ] [])
+
+        Links xs ->
+            xs |> Array.get i |> Maybe.map (\href -> H.a [ A.href href ] [])
+
+        Datepickers xs ->
+            -- TODO
+            Nothing
+
+        Checkboxes xs ->
+            -- TODO
+            Nothing
+
+        Sliders xs ->
+            -- TODO
+            Nothing
+
+        Fields xs ->
+            -- TODO
+            Nothing
+
+        Chart xs ->
+            -- TODO
+            Nothing
+
+
+viewSheet : Sheet -> Html Msg
+viewSheet sheet =
+    H.table []
+        [ H.thead []
+            [ H.tr [] <|
+                Array.toList <|
+                    Array.map (H.th [] << ls << text << Tuple.first) <|
+                        sheet.cols
+            ]
+        , H.tbody [] <|
+            Array.toList <|
+                Array.initialize sheet.rows
+                    (\i ->
+                        H.tr [] <|
+                            Array.toList <|
+                                Array.initialize (Array.length sheet.cols)
+                                    (H.td [] << ls << Maybe.withDefault (text "") << Maybe.andThen (viewCell i) << Maybe.map Tuple.second << flip Array.get sheet.cols)
+                    )
+        ]
+
+
+view : Model -> Browser.Document Msg
+view model =
+    { title = "scrapsheets"
+    , body =
+        [ H.node "style" [] [ text "" ]
+        , H.main_ []
+            [ H.div [ S.displayFlex, S.flexDirectionColumn ] <|
+                List.map (H.div [ S.displayFlex, S.flexDirectionRow ]) <|
+                    List.map
+                        (List.map
+                            (Result.withDefault (H.div [] [ text "TODO: error" ])
+                                << Result.map viewSheet
+                                << Result.andThen .sheet
+                                << Result.fromMaybe "Sheet not found."
+                                << flip Dict.get model.sheets
+                            )
+                        )
+                    <|
+                        model.shelf
+            , H.aside []
+                []
+            ]
+        ]
+    }
