06d3c3c85ab584aa50945badef6d3852d8c2e068
Taylor Troesh <surprisetalk@gmail.com>
2025-08-11 16:54:11 -0700

    ♓

--- src/Main.elm
+++ src/Main.elm
@@ -1091,43 +1091,46 @@ view ({ sheet } as model) =
         , H.node "style" [] [ text "table { background: #fff; line-height: 1; }" ]
         , H.node "style" [] [ text "td a, td button { opacity: 0.8; }" ]
         , H.node "style" [] [ text "td a:hover, td button:hover { opacity: 1; }" ]
-        , H.node "style" [] [ text "th, td { padding: 0.25rem; padding-bottom: 0.15rem; font-weight: normal; border: 1px solid #888; height: 0.8rem; vertical-align: top; }" ]
+        , H.node "style" [] [ text "th, td { padding: 0.25rem; padding-bottom: 0.15rem; font-weight: normal; border: 1px solid #aaa; height: 0.8rem; vertical-align: top; }" ]
         , H.node "style" [] [ text "tr > :first-child { border-left: none; padding-left: 0.5rem; }" ]
         , H.node "style" [] [ text "tr > :last-child { border-right: none; padding-right: 0.5rem; }" ]
         , H.node "style" [] [ text "th > *, td > * { max-height: 6rem; text-overlow: ellipsis; }" ]
         , H.node "style" [] [ text "td:hover { background: rgba(0,0,0,0.025); }" ]
         , H.node "style" [] [ text ".r0 { position: sticky; top: -1px; background: #f6f6f6; z-index: 1; border-bottom: 0px; }" ]
-        , H.node "style" [] [ text ".r0::after { content: \"\"; display: block; position: absolute; width: 100%; left: 0; bottom: -1px; border-bottom: 1px solid #888; }" ]
+        , H.node "style" [] [ text ".r0::after { content: \"\"; display: block; position: absolute; width: 100%; left: 0; bottom: -1px; border-bottom: 1px solid #aaa; }" ]
         , H.node "style" [] [ text ".selected { background: rgba(0,0,0,0.05); }" ]
         , H.node "style" [] [ text "#code { font-family: monospace; background: #fff; }" ]
         , H.node "style" [] [ text "@media (min-width: 768px) { body > div { grid-template-columns: 1fr auto; } #aside { border-left: 1px solid #000; } }" ]
         , H.node "style" [] [ text "@media (max-width: 768px) { body > div { grid-template-rows: 1fr auto; } #aside { border-top: 1px solid #000; } }" ]
+        , H.node "style" [] [ text "@media (max-width: 768px) { #title { display: none; } }" ]
 
         -- , H.node "style" [] [ text "thead tr td { position: sticky; top: 0; }" ]
         -- , H.node "style" [] [ text "tfoot tr:last-child td { position: sticky; bottom: 0; }" ]
         , H.div [ S.displayGrid, S.gapRem 0, S.userSelectNone, S.cursorPointer, A.style "-webkit-user-select" "none", S.maxWidth "100vw", S.maxHeight "100vh", S.height "100%", S.width "100%" ]
             [ H.main_ [ S.displayFlex, S.flexDirectionColumn, S.width "100%", S.overflowXAuto, S.gapRem 0 ]
-                [ H.div [ S.displayFlex, S.flexDirectionRow, S.justifyContentSpaceBetween, S.gapRem 0, S.borderBottom "1px solid #000" ]
-                    [ H.div [ S.displayFlex, S.flexDirectionRow, S.alignItemsCenter, S.whiteSpaceNowrap, S.gapRem 0.5, S.padding "0.75rem 0 0.75rem 0.5rem" ] <|
-                        List.concat
-                            [ [ H.a [ A.href "/", S.fontWeight "900", S.fontSizeRem 1.5, S.heightRem 1, S.lineHeight "0.55", S.marginRightRem -0.25 ] [ text "⊞" ]
-                              , H.a [ A.href "/", S.fontWeight "900" ] [ text "scrapsheets" ]
-                              , text "/"
-                              ]
-                            , iif (sheet.id == "")
-                                [ H.span [] [ text "library" ]
-                                ]
-                                [ H.a [ A.href "#settings" ] [ text (iif (String.trim info.name == "") "untitled" info.name) ]
-                                ]
-                            , [ text "/"
-                              ]
+                [ H.div [ S.displayFlex, S.flexDirectionRow, S.alignItemsCenter, S.whiteSpaceNowrap, S.gapRem 0.5, S.paddingRem 0.5, S.borderBottom "1px solid #000", S.background "#f0f0f0" ] <|
+                    List.concat
+                        [ [ H.a [ A.href "/", S.fontWeight "900", S.fontSizeRem 1.5, S.heightRem 1, S.lineHeight "0.55" ] [ text "⊞" ]
+                          , H.a [ A.href "/", S.fontWeight "900", A.id "title", S.marginLeftRem -0.25 ] [ text "scrapsheets" ]
+                          , text "/"
+                          ]
+                        , [ H.span [] [ text "anon" ]
+                          , text "/"
+                          ]
+                        , iif (sheet.id == "")
+                            [ H.span [] [ text "library" ]
                             ]
-
+                            [ H.a [ A.href "#settings" ] [ text (iif (String.trim info.name == "") "untitled" info.name) ]
+                            ]
+                        , [ H.a [ A.href "#", S.marginLeftAuto ] [ text "help" ]
+                          ]
+                        ]
+                , H.div [ S.displayFlex, S.flexDirectionRow, S.justifyContentSpaceBetween, S.gapRem 0, S.borderBottom "1px solid #aaa" ]
                     -- All current filters should be rendered as text in the searchbar.
                     -- This helps people (1) learn the language and (2) indicate that they're searching rather than editing.
                     -- TODO: If no results found, show saved searches and recent searches.
-                    , H.div [ S.displayFlex, S.width "100%", S.height "100%" ]
-                        [ H.input [ A.value model.search, A.onInput (InputChange SheetSearch), A.placeholder "search", S.width "100%", S.border "none", S.backgroundColor "#fff", S.paddingLeftRem 1 ] []
+                    [ H.div [ S.displayFlex, S.width "100%", S.height "100%" ]
+                        [ H.input [ A.value model.search, A.onInput (InputChange SheetSearch), A.placeholder "search", S.width "100%", S.border "none", S.backgroundColor "#fff", S.padding "0.25rem 0.5rem" ] []
                         ]
                     ]
 
